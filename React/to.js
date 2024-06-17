a902c31038ca75796109757dd421ea75e563b7e9
Yuvadi29 <adityatrivedi612@gmail.com>
2024-06-17 10:43:07 +0530

    JavaScript Concept Revision

---
+++ React/to.js
@@ -0,0 +1,19 @@
+// let arr1 = [1,2,3,4,5];
+// let arr2 = [3,4,5];
+
+// let arr3 = [...arr1, ...arr2];
+// console.log(arr3);
+
+const person = {
+    name: "harikrishnan",
+    age: 21,
+    place: "virar"
+}
+
+
+// let person2 = {...person, name: 'adi'}
+// console.log(person2)
+
+const {age, place} = person;
+console.log(age);
+console.log(place);
